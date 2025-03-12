fc9c59c46c92537e2693373850527dd461b8c629
Mary Joe <maryjoekhoury@outlook.com>
2025-03-12 09:35:58 +0200

    add colors for win/loose status

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,6 @@
 import random
-
+import colorama
+from colorama import Back, Fore, Style
 options=['rock','paper','scissors']
 
 user_score=0
@@ -7,40 +8,40 @@ opponent_score=0
 
 
 
-
+colorama.init(autoreset=True)
 while user_score<3 and opponent_score<3:
     opponent_option=random.choice(options)
     user_choice=input('Enter your choice(rock,paper,scissors): ')
     while user_choice.lower()!='paper' and user_choice.lower()!='rock' and user_choice.lower()!='scissors':
         user_choice=input('Wrong choice!\n Enter your choice:')
     if user_choice==opponent_option:
-        print(f'Draw! you both chose {user_choice}')
+        print(Fore.YELLOW+f'Draw! you both chose {user_choice}')
     elif  user_choice.lower()=='rock':
         if opponent_option=='scissors':
-            print(f'{opponent_option} \nYou win!')
+            print(Fore.GREEN+f'{opponent_option} \nYou win!')
             user_score+=1
         elif opponent_option=='paper':
-            print(f'You lost:( Opponent chose {opponent_option}')
+            print(Fore.RED +f'You lost:( Opponent chose {opponent_option}')
             opponent_score+=1
     elif user_choice.lower()=='paper':
         if opponent_option=="rock":
-            print(f'{opponent_option} \nYou win!')
+            print(Fore.GREEN+f'{opponent_option} \nYou win!')
             user_score+=1
         elif opponent_option=="scissors":
-            print(f'You lost:( Opponent chose {opponent_option}')
+            print(Fore.RED+f'You lost:( Opponent chose {opponent_option}')
             opponent_score+=1
     
     else:
         if opponent_option=="paper":
-            print(f'{opponent_option} \nYou win!')
+            print(Fore.GREEN+f'{opponent_option} \nYou win!')
             user_score+=1
         elif opponent_option=="rock":
-            print(f'You lost:( Opponent chose {opponent_option}')
+            print(Fore.RED+f'You lost:( Opponent chose {opponent_option}')
             opponent_score+=1
     if user_score==3:
-        print(f'YOU WON!\n USER SCORE:{user_score}      OPPONENT SCORE:{opponent_score}')
+        print(Fore.GREEN+f'YOU WON!\nUSER SCORE:{user_score}      OPPONENT SCORE:{opponent_score}')
     elif opponent_score==3:
-        print(F'HARDLUCK:/ YOU LOST\n USER SCORE:{user_score}      OPPONENT SCORE:{opponent_score}')
+        print(Fore.RED+f'HARDLUCK:/ YOU LOST\nUSER SCORE:{user_score}      OPPONENT SCORE:{opponent_score}')
 
 
 
